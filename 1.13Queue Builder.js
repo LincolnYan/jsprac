51f42f3542f90ffe1f69f7a8e93fde809e73cb00
LincolnYan <17105073@student.curtin.edu.au>
2016-09-07 19:39:47 +0800

    Update 1.13Queue Builder.js

--- 1.13Queue Builder.js
+++ 1.13Queue Builder.js
@@ -8,6 +8,7 @@ var start = 2;
 
 // build applyAndEmpty function expression here
 var applyAndEmpty = function(input, queue){
+//Your loop parameters are very important. Consider what might happen if your queue begins to shrink at the same time that a loop condition checks the length of the queue’s array? You also wouldn’t want to hard code the length as a number, since you wouldn’t be able to reuse your function on other queues with different lengths. Think of a storage solution prior to the loop.
 	var length = queue.length;
 	for (var i = 0; i < length; i++) {
 		input = queue.shift()(input);
