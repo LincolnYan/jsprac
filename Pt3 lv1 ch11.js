f9213f7ed727d4805cb73a28eb70d4b3ecc8d79f
LincolnYan <17105073@student.curtin.edu.au>
2016-09-07 18:53:51 +0800

    Add files via upload

---
+++ Pt3 lv1 ch11.js
@@ -0,0 +1,12 @@
+function adventureSelector(userChoice) {
+  	if(userChoice == 1){
+  		return alert("You select the Vines of Doom!");};
+  	}else if(userChoice == 2) {
+  		return function (){alert("Looks like you want the Lake of Despair!");};
+  	}else if (userChoice == 3) {
+  		return function(){alert("The Caves of Catastrophe!");};
+  	}
+  	
+  
+
+}
